a4a2a121d857c5cf2e9cd92bf78c1ab20b67b844
Matthew Espinoza <112516041+Espi5730@users.noreply.github.com>
2024-09-14 04:00:01 +0000

    more work done

--- src/main.cpp
+++ src/main.cpp
@@ -35,21 +35,39 @@ int main () {
   const string HEADING = "HEX-DECIMAL-BINARY CONVERTER";
   const string PROMPT = "Enter your string to convert (q to quit): ";
   const char QUIT = 'q';
+  const char BINARY = 'B';
+  const char HEXADECIMAL = 'H';
+  const char DECIMAL = 'D';
 
   //vars
-
   string userInput;
+  char currentBase;
   int inputLength = 0;
 
   printTitle(HEADING);
-
   userInput = getNumber(PROMPT);
-
   inputLength = userInput.length();
-  cout << inputLength;
+
+  //main block
   
   while (!(QUIT == userInput[0] && 1 == inputLength))
   {
+
+    currentBase = getbase(userInput);
+
+    if (BINARY == currentBase)
+    {
+      cout << "is binary";
+    }
+    else if (HEXADECIMAL == currentBase) {
+      cout << "is hexa";
+    }
+    else {
+      cout << "is decimal";
+    }    
+
+    cout << endl << currentBase <<endl;
+
     userInput = getNumber(PROMPT);
     inputLength = userInput.length();
     cout << inputLength;
@@ -103,11 +121,55 @@ string getNumber(const string& prompt) {
  Returned:    	string - user inputed string to convert
  ****************************************************************************/
 
-char getbase(const string& strNumber) {
+char getbase (const string& strNumber) {
+  //consts
+  const char INPUT_BINARY = 'b';
+  const char INPUT_HEXACDECIMAL = 'x';
 
-  char tmp = 'a';
-  // example code for splicing 
-  
+  const char RETURN_DECIMAL = 'D';
+  const char RETURN_HEXACDECIMAL = 'H';
+  const char RETURN_BINARY = 'B';
 
-  return tmp;
+  char userInput;
+
+  // if length is 1, return decimal
+
+  if (strNumber.length () == 1) {
+    return RETURN_DECIMAL;
+  } else {
+    // if length greater then 2, check at second index
+    userInput = strNumber[1];
+    cout << "this is" << userInput;
+    if (INPUT_HEXACDECIMAL == userInput)
+    {
+      return RETURN_HEXACDECIMAL;
+    } 
+    else if (INPUT_BINARY == userInput) {
+      return RETURN_BINARY;
+    } 
+    else {
+      return RETURN_DECIMAL;
+    }
+  }
+}
+
+/****************************************************************************
+ Function:    	hexCharToInt
+
+ Description: 	returns Decimal equivalent to Hex Digit Input
+
+ Parameters:  	hexDigit - inputed hex digit
+ 
+ Returned:    	int 
+ ****************************************************************************/
+int hexCharToInt (char hexDigit) {
+  const int LOWER_DECIMAL_ASCII = 48;
+  const int UPPER_DECIMAL_ASCII = 57;
+  int returnInt = 0;
+
+  if (hexDigit > LOWER_DECIMAL_ASCII && hexDigit < UPPER_DECIMAL_ASCII) {
+    cout << "is decimal";
+  }
+
+  return returnInt;
 }
